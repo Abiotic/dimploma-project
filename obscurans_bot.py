01f871f14e1483c383f92c134325922f8dcd1e49
Abiotic <spotless.alice@gmail.com>
2021-01-18 15:27:07 +0300

    fix problems

--- obscurans_bot.py
+++ obscurans_bot.py
@@ -2,9 +2,7 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ReplyKeyboardMarkup
 import wikipedia
-import wikipediaapi
 import settings
-import time
 import jellyfish as jf
 
 #wikipediaapi.log.setLevel(level=wikipediaapi.logging.DEBUG)
@@ -25,14 +23,12 @@ def greet_user(update, context):
 
 
 def talk_to_me(update,context):
-	user_text = update.message.text
-	comparison_res = input_comparison(user_text)
+	user_input = update.message.text
+	comparison_res = input_comparison(user_input)
 	if comparison_res == None:
 		update.message.reply_text('Попробуйте еще раз')
-	#print(f'kek + {comparison_res}')
 	else:
 		answer = wiki_search(comparison_res)
-		#print(answer)
 		update.message.reply_text(answer)
 
 
@@ -40,7 +36,6 @@ def main():
 	mybot = Updater(settings.API_KEY,use_context = True)
 	dp = mybot.dispatcher
 	dp.add_handler(CommandHandler("start",greet_user))
-	#dp.add_handler(MessageHandler(Filters.regex('^(начнем)$'), greet_user))
 	dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 	logging.info("Bot was launched")
 	mybot.start_polling()
@@ -50,17 +45,22 @@ def main():
 def main_keyboard():
 	return ReplyKeyboardMarkup([['/start']])
 
-def input_comparison(text):
-	text = text.lower()
-	#threshold = 2
-	with open('conspiracy_obscurans_db.txt','r', encoding='utf-8') as f:
+def input_comparison(user_input):
+	user_input = user_input.lower()
+
+	with open(settings.THEORY_DB,'r', encoding='utf-8') as f:
 		for line in f:
 			line = line.lower().strip()
-			dld = jf.damerau_levenshtein_distance(text,line)
-			if dld < 0.15*len(line) or dld <= 2:
+			
+			if is_misprint_below_threshold(user_input, line):
 				return line 
 	return None
 
+def is_misprint_below_threshold (user_input, db_line):
+	allowed_misprints = 2
+	misprint_threshold = 0.15
+	distance = jf.damerau_levenshtein_distance(user_input,db_line)
+	return distance < misprint_threshold*len(db_line) or distance <= allowed_misprints
 
 
 def wiki_search(theory):
@@ -69,9 +69,9 @@ def wiki_search(theory):
 	w_page = wikipedia.page(theory)
 	for sec in w_page.sections:
 		if key_word in sec:
-			return f'бла бла (придумать текст) \n{w_page.section(sec)}'
-		else:
-			return f'Не смог найти для тебя критику, поэтому держи:\n{w_page.summary}'
+			return f'Критика: \n{w_page.section(sec)}'
+
+	return f'Не смог найти для тебя критику, поэтому держи описание:\n{w_page.summary}'
 
 
 
